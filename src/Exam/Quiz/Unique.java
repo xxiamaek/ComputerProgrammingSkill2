eb9e7e447c43e857c86d05daa01c3abc2816ca6a
Suphakorn Phasuk <suphakorn.ps@gmail.com>
2025-12-25 22:12:51 +0700

    Fix Quiz 1

--- src/Exam/Quiz/Unique.java
+++ src/Exam/Quiz/Unique.java
@@ -1,32 +1,16 @@
 package Exam.Quiz;
 
-import java.util.LinkedHashSet;
 import java.util.Scanner;
-import java.util.Set;
 
 public class Unique {
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
             int n = sc.nextInt();
-            
-            Set<Integer> numbers = new LinkedHashSet<>();
-
-            for (int i = 0; i < n; i++) {
-                int input = sc.nextInt();
-                numbers.add(input);
-            }
-
-            for (int num : numbers) {
-                System.out.print(num + " ");
-            }
-           
-            /* 
-            int n = sc.nextInt();
 
             int[] numbers = new int[n];
             int count = 0;
 
-            for (int i = 0; i < n; i++) {
+            for (int i=0 ; i < n ; i++) {
                 int input = sc.nextInt();
                 boolean isDuplicate = false;
 
@@ -46,7 +30,6 @@ public class Unique {
             for (int i = 0; i < count; i++) {
                 System.out.print(numbers[i] + " ");
             }
-            */
 
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
